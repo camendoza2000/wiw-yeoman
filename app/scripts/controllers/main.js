56d81abfe9d5f21be9959cd00dbfbf0e87b66261
Carlos Mendoza <camendoza@sciodev.com>
2014-11-12 11:03:47 -0600

    yeoman structure and Gruntfile.js commented line 166

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wiwYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wiwYeomanApp
+ */
+angular.module('wiwYeomanApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
